bd0fc4f491cc21e6bde95b1f28d1bf2e29d4d6e1
Vincent Racelle <vince@MacBook-Pro-de-Vincent.local>
2020-05-01 16:20:50 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# seba-the-artist
+
+Sanity studio is in the studio repo
+the site is in gatsby
+
+Run sanity studio : sanity start from folder
+Run gastby : gatsby develop from forler
+
---
+++ studio/schemas/expos.js
@@ -0,0 +1,48 @@
+export default {
+    title: 'expos',
+    name: 'Expos',
+    type: 'document',
+    fields: [
+        {
+            title: 'name',
+            name: 'name',
+            type: 'string',
+            validation: Rule => Rule.required()
+        },
+        {
+            title: 'description',
+            name: 'description',
+            type: 'text',
+            max: 500
+        },
+        {
+            title: 'date',
+            name: 'date',
+            type: 'datetime',
+            validation: Rule => Rule.required()
+        },
+        {
+            title: 'Slider Image',
+            name: 'sliderImage',
+            type: 'image',
+            validation: Rule => Rule.required()
+        },
+        {
+            title: 'lieu',
+            name: 'place',
+            type: 'geopoint'
+        },
+        {
+            title: 'Slug',
+            name: 'slug',
+            type: 'slug',
+            options: {
+                source: 'name',
+                slugify: input => input
+                    .toLowerCase()
+                    .replace(/\s+/g, '-')
+                    .slice(0, 200)
+            }
+        }
+    ],
+}
