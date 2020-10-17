1cc2d69214eca49134650167c61fb26c4e95f770
Vincent <vracelle@adneom.lu>
2020-10-17 09:23:45 +0200

    put windo in useeffect

--- site/src/components/gallery/gallery.js
+++ site/src/components/gallery/gallery.js
@@ -24,7 +24,7 @@ const renderImages = (images, handleFullScreen, assetName) =>
 
 const Gallery = ({ images, handleFullScreen, assetName }) => {
   const [height, setHeight] = useState(380)
-  const [windowWidth, setWindowWidth] = useState()
+  const [windowWidth, setWindowWidth] = useState(null)
 
   useEffect(() => {
     setWindowWidth(window.innerWidth)
