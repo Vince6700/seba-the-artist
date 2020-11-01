60544a348d82bda70b6db65b48684d08595f570b
Vincent <vracelle@adneom.lu>
2020-11-01 16:55:59 +0100

    version 1.0

---
+++ studio/src/dashboardConfig.js
@@ -0,0 +1,18 @@
+export default {
+  widgets: [
+    {
+      name: 'netlify',
+      options: {
+        title: 'My Netlify deploys',
+        sites: [
+          {
+            title: 'Sanity Studio',
+            apiId: 'AAAAB3NzaC1yc2EAAAADAQABAAACAQCqT6hmkidsl/MyEL7DOy6l13vZ4YSZ6uzNumKagBMs25Xaw76gify87yu2mhIr1JMVN2REWS5mRRJrRSRoeZlRj/p814fjMC0Dc5mjxbthtdaok+nn4wV8yfN4AuwzAk+XbD9iDgQs5wvFBUTcFmc86fyUR2hnAmmECHb3goANNjjMj/JMaEWCTDPdVWqG+IIoMuPJghSjt/MH/urvYQsC1FdvUL+Hz9H/zWvxfWfv7jXu1k7B+KlqhKIiRFIXW6cxx6IFgi0muIO6kqsriZRhTpSvm4dfeGyxoyYovpMQebCtOX0/Vq0GCXtQK2kXKWtCylI+s+d5AhGxnmYkmUxYaOJ3NZHxBCiRH4CHc/XcUL/jPDjimw5SwpSkvhkXfgEMGHC4p0LhdLMzUZ1XMXmDR13uE4DnAftD2fR1n0szQlcYKKdb2HvKLzBIZEBluqjCUysS502ZUAElCMA//9il42FQvY3Qf8W3ce+kZWS0RMpOt4lr51o//gUAUiwpd/nnGC5sFLrGGM2NhF/RSFMPbA3hKW/Z1ju1BiRf5VhLWFmTLiaLVWMOOtanGBZaai6RFuohcVoOTXTQPxDj4MXh8J31d3Nf9GcksnW7SGfZQeGyOf5iBo++78ywplF+r8EITCsUL9RBZ2945Grk6lZLVuRJbk04Jj8IvvthTm8lww==',
+            buildHookId: '5f9ed6e936a6753981d11af4',
+            name: 'affectionate-engelbart-4dbd94',
+          }
+        ]
+      }
+    }
+  ]
+}
